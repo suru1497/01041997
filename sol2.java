e12b451b0bd01ba0fb0bc410375d98c385354593
surbhi1497 <44054450+surbhi1497@users.noreply.github.com>
2018-10-16 00:47:07 +0530

    Create sol2.java

---
+++ sol2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+			
+	}
+}
+		
