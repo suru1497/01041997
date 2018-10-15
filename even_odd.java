4fbbe05bf2bcea33ae81a5b7323a124da6e7fc0f
surbhi1497 <44054450+surbhi1497@users.noreply.github.com>
2018-10-16 01:22:40 +0530

    Create even_odd.java

---
+++ even_odd.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		if(n<0)
+		
+				System.out.println("invalid");
+		else
+		if(n%2==0)
+	
+			System.out.println("Even");
+		
+		else
+			System.out.println("Odd");
+			
+			
+			}
+}
+		
+		
