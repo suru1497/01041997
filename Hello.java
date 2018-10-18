d669554768c7578827a701ab9ab133548d6e9ecb
surbhi1497 <44054450+surbhi1497@users.noreply.github.com>
2018-10-18 21:30:42 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Hello{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);	
+		int n=s.nextInt();
+		for(int i=0;i<n;i++){
+			System.out.println("Hello");
+		}
+	}
+}
