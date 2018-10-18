05db0ad9128985b4643aa2a8a323f11248dc2610
surbhi1497 <44054450+surbhi1497@users.noreply.github.com>
2018-10-18 21:35:29 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Natural{
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);	
+		int n=s.nextInt();
+		int result=n*(n+1)/2;
+		System.out.println(result);
+	}
+}
