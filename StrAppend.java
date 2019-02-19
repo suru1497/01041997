3c88a51ddcf2fba0c613f781066eb292b236c0b6
surbhi1497 <44054450+surbhi1497@users.noreply.github.com>
2019-02-19 21:30:14 +0530

    Create StrAppend.java

---
+++ StrAppend.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class StrAppend{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		String s=".";
+		String newstr=str+""+s;
+		System.out.println(newstr);
+	}
